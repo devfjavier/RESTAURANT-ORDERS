a937ebdc0ec1477f97401d64177144a7ada57f60
javier <devjavicont@gmail.com>
2021-08-31 12:35:35 -0400

    se creo nuevo archivo js para los productos y a ese archivo se paso los productos que estaban en scripts.js

---
+++ JS/products.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const products = [
+    {
+        name: "Hamburguesa simple",
+        cost: 20,
+        code: "H100"
+    },
+    {
+        name: "Hamburguesa doble",
+        cost: 30,
+        code: "H200"
+    },
+    {
+        name: "Papas fritas",
+        cost: 8,
+        code: "P100"
+    },
+    {
+        name: "Refresco personal",
+        cost: 6,
+        code: "R100"
+    }
+]
--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,4 @@
 "user strict";
-const products = [
-    {
-        name: "Hamburguesa simple",
-        cost: 20,
-        code: "H100"
-    },
-    {
-        name: "Hamburguesa doble",
-        cost: 30,
-        code: "H200"
-    },
-    {
-        name: "Papas fritas",
-        cost: 8,
-        code: "P100"
-    },
-    {
-        name: "Refresco personal",
-        cost: 6,
-        code: "R100"
-    }
-]
 
 const showMenu = () => {
     console.log("CÓDIGO - NOMBRE DEL PRODUCTO - COSTO")

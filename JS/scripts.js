fde4c59201fc29f83bfd0773d64162630f26e576
javier <devjavicont@gmail.com>
2021-08-31 12:57:31 -0400

    se agrego la funcion de perdir productos

--- JS/scripts.js
+++ JS/scripts.js
@@ -16,3 +16,14 @@ const showMenu = () => {
     }
 }
 
+const orderProduct = cod => {
+    if (!cod) return "Ingrese un codigo valido"
+
+    const productFound = products.find(product => product.code === cod)
+    if (!productFound) return "El producto no existe"
+    
+    order.push(productFound)
+    
+    console.log("Su producto ha sido agregado a su pedido. Su pedido es:")
+    return order
+} 

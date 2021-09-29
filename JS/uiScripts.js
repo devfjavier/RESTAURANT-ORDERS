502bb78db896f1676797efc05ef08a13f8769069
javier <devjavicont@gmail.com>
2021-09-28 22:24:35 -0400

    se agrego una funcion para renderizar los pedidos

--- JS/uiScripts.js
+++ JS/uiScripts.js
@@ -13,7 +13,30 @@ function loadJS() {
             const productFound = products.find(prod => prod.code === button.id)
 
             order.push(productFound)
+            renderProduct(productFound.name, productFound.code, productFound.cost)
         })
     })
+
+    const renderProduct = (name, code, cost) => {
+        const   orderList = document.getElementById("order-list")
+        const   order = document.createElement("li"),
+                nameElement = document.createElement("h3"),
+                codeElement = document.createElement("span"),
+                costElement= document.createElement("span")
+
+        order.classList.add("order")
+        nameElement.classList.add("title")
+        nameElement.textContent = name
+        codeElement.textContent = code
+        costElement.textContent = `${cost}$`
+
+        order.innerHTML = `
+            ${nameElement.outerHTML}
+            ${codeElement.outerHTML}
+            ${costElement.outerHTML}
+        `
+
+        orderList.appendChild(order)
+    }
 
 }

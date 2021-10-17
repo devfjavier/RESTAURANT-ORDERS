a8fab1f3c3e01100ac7e471a3b2476ec2fb79394
javier <devjavicont@gmail.com>
2021-10-17 17:31:52 -0400

    el costo del pedido ahora es reflejado en la interfaz de usuario en "Precio total"

--- JS/orderProduct.js
+++ JS/orderProduct.js
@@ -7,13 +7,19 @@ let order = []
 
 
 const orderProduct = cod => {
+    
     // Validar que el dato cod sea string
     if (!cod || typeof cod === "number" || typeof cod === "boolean") return "Ingrese un codigo valido"
+    
+    const productsNumber = document.getElementById("products-number")
+    const totalPrice = document.getElementById("total-price")
 
     const productFound = products.find(product => (product.code === cod))
     if (!productFound) return "El producto no existe"
     
     order.push(productFound)
+    productsNumber.textContent = order.length
+    totalPrice.textContent = calculateCost()
     renderOrder(productFound.name, productFound.code, productFound.cost)
     
     console.log("El producto ha sido agregado a su pedido. Su pedido es:")
@@ -30,6 +36,7 @@ const seeOrder = () => order
 addEventListener("DOMContentLoaded", () => {
     const orderProductButtons = Array.from(document.querySelectorAll(".order-product"))
     const productsNumber = document.getElementById("products-number")
+    const totalPrice = document.getElementById("total-price")
     
     for (const button of orderProductButtons) {
         button.addEventListener("click", () => {
@@ -37,6 +44,7 @@ addEventListener("DOMContentLoaded", () => {
 
             if (productFound) {
                 order.push(productFound)
+                totalPrice.textContent = calculateCost()
                 productsNumber.textContent = order.length
                 console.log("El producto ha sido agregado a su pedido. Su pedido es:")
                 renderOrder(productFound.name, productFound.code, productFound.cost)
